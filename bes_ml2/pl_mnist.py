d7318fb757ec76fe0e41f0f27aafbd18a2877f9c
David R. Smith <david.smith@wisc.edu>
2023-03-30 15:42:00 -0400

    mods

--- bes_ml2/pl_mnist.py
+++ bes_ml2/pl_mnist.py
@@ -1,5 +1,4 @@
 import dataclasses
-import time
 
 import torch
 import torch.nn
@@ -8,10 +7,10 @@ import torch.utils.data
 import torchvision.datasets
 import torchvision.transforms
 
-import pyhessian
-
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+from pytorch_lightning import callbacks as cb
+from pytorch_lightning.utilities.model_summary import ModelSummary
 
 import wandb
 
@@ -20,8 +19,8 @@ class MNISTDataModule_DataClass():
     data_dir: str = './MNIST'
     batch_size: int = 32
     fraction_validation: float = 0.2
-    seed: int = 42
     num_workers: int = 8
+    seed: int = 42
 
 
 @dataclasses.dataclass(eq=False)
@@ -31,23 +30,25 @@ class MNISTDataModule(
 ):
     def __post_init__(self):
         super().__init__()
+        self.save_hyperparameters()
+
+    def prepare_data(self):
+        for train in [True, False]:
+            torchvision.datasets.MNIST(
+                self.data_dir, 
+                download=True,
+                train=train,
+            )
 
     def setup(self, stage: str):
+        # train and validation data
         transform = torchvision.transforms.ToTensor()
         mnist_full = torchvision.datasets.MNIST(
             self.data_dir, 
-            download=True,
             train=True,
             transform=transform,
         )
-        self.mnist_test = torchvision.datasets.MNIST(
-            self.data_dir, 
-            download=True,
-            train=False,
-            transform=transform,
-        )
-
-        # partition training and validation data
+        # partition
         valid_set_size = int(len(mnist_full) * self.fraction_validation)
         train_set_size = len(mnist_full) - valid_set_size
         seed = torch.Generator().manual_seed(self.seed)
@@ -55,6 +56,13 @@ class MNISTDataModule(
             mnist_full, 
             [train_set_size, valid_set_size], 
             generator=seed,
+        )
+
+        # test data
+        self.mnist_test = torchvision.datasets.MNIST(
+            self.data_dir, 
+            train=False,
+            transform=transform,
         )
 
     def train_dataloader(self):
@@ -81,11 +89,13 @@ class MNISTDataModule(
 @dataclasses.dataclass(eq=False)
 class LitAutoEncoder_DataClass():
     lr: float = 1e-3
+    weight_decay: float = 1e-3
     hidden_size_1: int = 128
     hidden_size_2: int = 32
-    hessian_epoch_interval: int = 2
     dropout: float = 0.1
     leaky_relu_slope: float = 1e-2
+    scheduler_patience: int = 2
+    scheduler_threshold: float = 1e-3
 
 @dataclasses.dataclass(eq=False)
 class LitAutoEncoder(
@@ -95,7 +105,7 @@ class LitAutoEncoder(
     
     def __post_init__(self):
         super().__init__()
-        self.example_input_array = torch.Tensor(4,28,28)
+        self.example_input_array = torch.Tensor(1, 28,28)
         self.save_hyperparameters()
         self.classifier = torch.nn.Sequential(
             torch.nn.Flatten(),
@@ -108,7 +118,6 @@ class LitAutoEncoder(
             torch.nn.Linear(self.hidden_size_2, 10),
             torch.nn.Softmax(dim=1),
         )
-        self.loss_fn = F.cross_entropy
 
     def forward(self, x: torch.Tensor):
         return self.classifier(x)
@@ -116,7 +125,7 @@ class LitAutoEncoder(
     def eval_and_loss(self, batch):
         x, y = batch
         y_hat = self(x)
-        loss = self.loss_fn(y_hat, y)
+        loss = F.cross_entropy(y_hat, y)
         return loss
 
     def training_step(self, batch, batch_idx) -> torch.Tensor:
@@ -133,75 +142,56 @@ class LitAutoEncoder(
         self.log("hp_metric", loss)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
-
-    # def on_train_epoch_end(self) -> None:
-    #     if self.hessian_epoch_interval and self.current_epoch%self.hessian_epoch_interval == 0:
-    #         print('calc hess')
-    #         t_start = time.time()
-    #         hessian = pyhessian.hessian(
-    #             model=self,
-    #             criterion=self.loss_fn,
-    #             dataloader=self.trainer.train_dataloader,
-    #             cuda=torch.cuda.is_available(),
-    #         )
-    #         print('calc eigenvals')
-    #         # eigenvalues, eigenvectors = hessian.eigenvalues(top_n=1, maxIter=5, tol=1e-2)
-    #         # eigen_list, weight_list = hessian.density(iter=5)
-    #         # print(eigen_list)
-    #         # print(weight_list)
-    #         # print(f"elapsed time {time.time()-t_start:.1f} s")
-    #         eigen_vals, eigen_vecs = hessian.eigenvalues(maxIter=10, top_n=5)
-    #         print(eigen_vals)
-    #         eigen_list, weight_list = hessian.density(iter=10)
-    #         print(eigen_list)
-    #         print(weight_list)
-    #         print('finished hess')
-    #         print(f"elapsed time {time.time()-t_start:.1f} s")
-
-    #         for param in self.parameters():
-    #             param.grad = None
+        optimizer = torch.optim.SGD(
+            self.parameters(), 
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+        )
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer=optimizer,
+                factor=0.5,
+                patience=self.scheduler_patience,
+                threshold=self.scheduler_threshold,
+                min_lr=1e-5,
+            ),
+            'monitor': 'val_loss',
+        }
 
 
 def dev_test():
     # define model
-    autoencoder = LitAutoEncoder(
-        hessian_epoch_interval=1,
-    )
-    datamodule = MNISTDataModule()
+    model = LitAutoEncoder()
+    print(ModelSummary(model, max_depth=-1))
+    dm = MNISTDataModule(num_workers=1)
 
     # check model and data
     check_trainer = pl.Trainer(
         fast_dev_run=True,
         enable_progress_bar=False,
-        devices=1,
+        enable_model_summary=False,
     )
-    check_trainer.fit(
-        model=autoencoder, 
-        datamodule=datamodule,
-    )
-    check_trainer.test(
-        model=autoencoder,
-        datamodule=datamodule
-    )
+    check_trainer.fit(model, dm)
+    check_trainer.test(model, dm)
 
 
 def full_test():
     exp_dir = 'Experiment_test'
 
-    autoencoder = LitAutoEncoder(
-        lr=1e-4,
-        hessian_epoch_interval=0,
-    )
-    datamodule = MNISTDataModule()
+    model = LitAutoEncoder(scheduler_threshold=5e-2, dropout=0.2)
+    print(ModelSummary(model, max_depth=-1))
+    dm = MNISTDataModule()
 
+    # set loggers
+    loggers = []
     tb_logger = TensorBoardLogger(
         save_dir='.', 
         name=exp_dir, 
         log_graph=True,
     )
-    tb_logger.log_graph(autoencoder)
+    tb_logger.log_graph(model)
+    loggers.append(tb_logger)
 
     wandb.login()
     wandb_logger = WandbLogger(
@@ -209,28 +199,33 @@ def full_test():
         project=exp_dir,
         name=f"version_{tb_logger.version}"
     )
-    wandb_logger.watch(autoencoder, log='all', log_freq=500)
+    wandb_logger.watch(model, log='all', log_freq=500)
+    loggers.append(wandb_logger)
 
+    # set callbacks
+    callbacks = [
+        cb.LearningRateMonitor(),
+        cb.EarlyStopping(
+            monitor='val_loss',
+            min_delta=1e-3,
+            patience=3,
+        ),
+    ]
+
+    # do train
     trainer = pl.Trainer(
-        max_epochs=6,
-        logger=[
-            tb_logger,
-            wandb_logger,
-        ],
-        # enable_progress_bar=False,
+        max_epochs=12,
+        gradient_clip_val=0.05,
+        enable_model_summary=False,
+        logger=loggers,
+        callbacks=callbacks,
     )
-    trainer.fit(
-        model=autoencoder, 
-        datamodule=datamodule,
-    )
-    trainer.test(
-        model=autoencoder, 
-        datamodule=datamodule,
-    )
+    trainer.fit(model, datamodule=dm)
+    trainer.test(model, datamodule=dm)
 
     wandb.finish(quiet=True)
 
 
 if __name__=="__main__":
-    dev_test()
-    # full_test()
+    # dev_test()
+    full_test()
