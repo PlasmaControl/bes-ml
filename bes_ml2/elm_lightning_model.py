d3efef87067657adce25041ca487f7ba28be4220
David Smith <david.smith@wisc.edu>
2023-05-08 14:24:48 -0700

    mods

--- bes_ml2/elm_lightning_model.py
+++ bes_ml2/elm_lightning_model.py
@@ -38,14 +38,14 @@ class Lightning_Model_Dataclass(
 
 @dataclasses.dataclass(eq=False)
 class Lightning_Model(
-    Lightning_Model_Dataclass,
     LightningModule,
+    Lightning_Model_Dataclass,
 ):
 
     def __post_init__(self):
         print(self.__class__.__mro__)
-        super().__init__()
-        self.torch_model = None
+        super(Lightning_Model_Dataclass,self).__init__()
+        # self.torch_model = None
 
     def set_torch_model(self, torch_model: torch.nn.Module):
         assert hasattr(torch_model, 'signal_window_size')
